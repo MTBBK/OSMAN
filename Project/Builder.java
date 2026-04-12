5e329c02d4d188ed2570187892b3f54e6cb2e956
Sunitatap <melihtursun@gmail.com>
2026-04-12 18:01:57 +0300

    Updated Builder.java
    -Moved theme name detection from Builder to ThemeNameStrategy
    -Added a log system to track what is going on in which step of the process.
    -Found out that makeFile and SocialLinksStrategy are not working as intended. Fix is needed.
    -Added ability to skip to NonArrayStrategy as it is required to prevent unused variables crashing the system.

--- Project/Builder.java
+++ Project/Builder.java
@@ -13,9 +13,12 @@ public class Builder {
 
         try {
             // sets error's print location to log.txt in ErrorLogs
-            writeFile("Project/ErrorLogs/log.txt", null);
-            PrintStream err = new PrintStream(new FileOutputStream("Project/ErrorLogs/log.txt"));
+            writeFile("OSMAN/Project/ErrorLogs/errorLog.txt", null);
+            PrintStream err = new PrintStream(new FileOutputStream("OSMAN/Project/ErrorLogs/errorLog.txt"));
             System.setErr(err);
+            writeFile("OSMAN/Project/ErrorLogs/log.txt", null);
+            PrintStream out = new PrintStream(new FileOutputStream("OSMAN/Project/ErrorLogs/log.txt"));
+            System.setOut(out);
 
             // begin site building process
             buildSite();
@@ -26,32 +29,24 @@ public class Builder {
     }
 
     static void buildSite() throws Exception {
+        System.out.println("buildFile: Begin.");
         // this string will be specified in config.toml later on
-        String config = readFile("Project/config.toml");
-        System.out.println(config);
-        // index of THEME_NAME in the config file.
-        int THEME_NAMEindex = config.indexOf("THEME_NAME");
-        if (-1 == THEME_NAMEindex) {
-            throw new Exception("Failed to find \"THEME_NAME\" in the config file.");
-        }
-        // index of the first " after THEME_NAME
-        int kesme1Index = config.indexOf('"', THEME_NAMEindex);
-        // selected theme's name
-        // "Project/Themes/" + themeName + "/" + themeName + ".css"
-        String themeName = config.substring(kesme1Index + 1, config.indexOf('"', kesme1Index + 1));
-        if (!(new File("Project/Themes/" + themeName + "/" + themeName + ".css")).exists()) {
-            throw new Exception("Selected theme could not be found in Themes folder.");
-        }
+        String config = readFile("OSMAN/Project/config.toml");
+        System.out.println("buildSite: Successfully read the config.");
+
         // [i][0] - file name, [i][1] file content
-        // String[][] contents = parseContentFiles("Project/Content/Texts/");
-        String[][] templates = parseContentFiles("Project/Templates/");
+        // String[][] contents = parseContentFiles("OSMAN/Project/Content/Texts/");
+        String[][] templates = parseContentFiles("OSMAN/Project/Templates/");
+        System.out.println("buildSite: Successfully run parseContentFiles(\"OSMAN/Project/Templates/\").");
 
-        for (int i = 0; 0 != i; i++) {
-            ;
-            writeFile("Project/Output/" + templates[i][0], makeFile(templates[i], config).toString());
+        for (int i = 0; templates.length != i; i++) {
+            if (templates[i][0].equals("base.html")) {
+                writeFile("OSMAN/Project/Output/" + templates[i][0], makeFile(templates[i], config).toString());
+                System.out.println("buildSite: Successfully run makeFile on \"" + templates[i][0]
+                        + "\" and printed its output to Output folder.");
+            }
         }
-
-        throw new Exception("buildSite FONKSİYONU HAZIR DEĞİL");
+        System.out.println("buildFile: End.");
     }
 
     static String[][] parseContentFiles(String folderPath) throws IOException {
@@ -143,41 +138,53 @@ public class Builder {
     // replaces file[1]'s with the appropriate parts in config and returns the
     // result as a StringBuilder
     static StringBuilder makeFile(String[] file, String config) throws Exception {
+        System.out.println("\nmakeFile: Begin.");
         StringBuilder sbFile = new StringBuilder(file[1]);
         int index = 0;
 
         if (-1 != file[0].indexOf("base")) { // for base
             index = config.indexOf("BASE");
+            System.out.println("makeFile: Successfully found the start of base file's part.");
         } else if (-1 != file[0].indexOf("index")) { // for index
             index = config.indexOf("INDEX");
+            System.out.println("makeFile: Successfully found the start of index file's part.");
         } else { // for unknown templates
             // cut file[0] from 0 to first '.' to remove extension and make it all uppercase
             // then look for it in config
             index = config.indexOf(file[0].substring(0, file[0].indexOf('.')).toUpperCase());
+            System.out.println("makeFile: Successfully found the start of file's part.");
             if (-1 == index) {
                 throw new Exception("Could not find the mention in the config for the file \"" + file[0] + "\"");
             }
         }
 
         index = config.indexOf('\n', index) + 1;
-        while (-1 != index) {
-            int nextLine = config.indexOf('\n', index) + 1;
-            int nextDots = config.indexOf(config.indexOf(':', index));
-
+        int nextLine;
+        int nextColon = config.indexOf(':', index);
+        while (0 != index && -1 != nextColon) {
+            nextLine = config.indexOf('\n', index) + 1;
             // check if there is a variable to read and skip to the next iteration if there
             // isnt
-            if (nextDots > nextLine) {
+            if (nextColon > nextLine) {
                 index = nextLine;
+                nextColon = config.indexOf(':', index);
+                System.out.println("makeFile: Successfully skipped to the next line.");
                 continue;
+            } else {
+                String option = config.substring(index, nextColon);
+                System.out.println("makeFile: Successfully found the option: \"" + option + "\".");
+                setStrategy(option);
+                System.out.println("makeFile: Successfully performed setStrategy(\"" + option + "\") in the file.");
+                performStrategy(sbFile, config);
+                System.out.println("makeFile: Successfully performed performStrategy(\"" + option + "\") in the file.");
+                index = nextLine;
+                nextColon = config.indexOf(':', index);
+                System.out.println("makeFile: Successfully changed \"" + option + "\" in the file.");
             }
-
-            String option = config.substring(index, nextDots);
-            setStrategy(option);
-            performStrategy(sbFile, config);
-            index = nextLine;
         }
-
-        throw new Exception("makeFile METODU HAZIR DEĞİL");
+        System.out.println("makeFile: Successfully finished making the file.");
+        System.out.println("makeFile: End.\n");
+        return sbFile;
     }
 
     // takes current file's content, the part that will be changed and the new part
@@ -211,7 +218,9 @@ abstract class Strategy {
 class NavbarStrategy extends Strategy {
     @Override
     public void makeChanges(StringBuilder file, String config) throws Exception {
-        // TODO Auto-generated method stub
+        System.out.println("\nNavbarStrategy: Begin.");
+        System.out.println("NavbarStrategy: NavbarStrategy doesn't do anything yet.");
+        System.out.println("NavbarStrategy: End.\n");
     }
 }
 
@@ -220,6 +229,8 @@ class SocialLinksStrategy extends Strategy {
     public void makeChanges(StringBuilder file, String config) throws Exception {
         // <a href="{{ SOCIAL_ICONS }}"><img src="{{ SOCIAL_LINKS }}"
         // style="width:2rem;height:2rem;"></a>
+        System.out.println("\nSocialLinksStrategy: Begin.");
+        System.out.println("SocialLinksStrategy: Does not work as intended.");
 
         int lIndex = config.indexOf('\n', config.indexOf("SOCIAL_LINKS")) + 1;
         int iIndex = config.indexOf('\n', config.indexOf("SOCIAL_ICONS")) + 1;
@@ -275,46 +286,81 @@ class SocialLinksStrategy extends Strategy {
         }
 
         file.replace(file.indexOf("SOCIAL_LINKS"), file.indexOf("SOCIAL_LINKS") + 12, result.toString());
+        System.out.println("\nSocialLinksStrategy: End.\n");
     }
 }
 
 class ThemeNameStrategy extends Strategy {
     @Override
     public void makeChanges(StringBuilder file, String config) throws Exception {
-        // TODO Auto-generated method stub
+        System.out.println("\nThemeNameStrategy: Begin.");
+        int THEME_NAMEindex = config.indexOf(option);
+        if (-1 == THEME_NAMEindex) {
+            throw new Exception("Failed to find \"THEME_NAME\" in the config file.");
+        }
+        // index of the first " after THEME_NAME
+        int kesme1Index = config.indexOf('"', THEME_NAMEindex);
+        // selected theme's name
+        // "Project/Themes/" + themeName + "/" + themeName + ".css"
+        String themeName = config.substring(kesme1Index + 1, config.indexOf('"', kesme1Index + 1));
+        themeName = "OSMAN/Project/Themes/" + themeName + "/" + themeName + ".css";
+        if (!(new File(themeName)).exists()) {
+            throw new Exception("Selected theme could not be found in Themes folder.");
+        }
+        option = "{{ " + option + " }}";
+        Builder.stringEditor(option, themeName, file);
+        System.out.println("ThemeNameStrategy: End.\n");
     }
 }
 
 class NonArrayStrategy extends Strategy {
     @Override
     public void makeChanges(StringBuilder file, String config) throws Exception {
+        System.out.println("\nNonArrayStrategy: Begin.");
         int firstQuote = config.indexOf('"', config.indexOf(option)) + 1;
         String value = config.substring(firstQuote, config.indexOf('"', firstQuote));
         option = "{{ " + option + " }}";
-        file.replace(file.indexOf(option), file.indexOf(option) + option.length(), value);
+        int fOptionIndex = file.indexOf(option);
+        if (-1 == fOptionIndex) {
+            System.out.println(
+                    "NonArrayStrategy: Could not find the option \"" + option + "\" in the file, it will be skipped.");
+            System.out.println("NonArrayStrategy: End.\n");
+            return;
+        } else {
+            file.replace(fOptionIndex, fOptionIndex + option.length(), value);
+            System.out.println("NonArrayStrategy: End.\n");
+        }
+
     }
 }
 
 class Factory {
     // factory to decide which strategy is selected in which scenario
     static Strategy decideStrategy(String option) throws Exception {
+        System.out.println("\nFactory: Begin.");
+
         Strategy strategy;
         switch (option) {
             case "NAV_BAR_LINKS":
                 strategy = new NavbarStrategy();
+                System.out.println("Factory: Chose NavbarStrategy.");
                 break;
             case "SOCIAL_LINKS":
                 strategy = new SocialLinksStrategy();
+                System.out.println("Factory: Chose SocialLinksStrategy.");
                 break;
             case "THEME_NAME":
                 strategy = new ThemeNameStrategy();
+                System.out.println("Factory: Chose ThemeNameStrategy.");
                 break;
             case "":
                 throw new Exception("Could not detect the next config variable.");
             default:
                 strategy = new NonArrayStrategy();
+                System.out.println("Factory: Chose NonArrayStrategy.");
         }
         strategy.option = option;
+        System.out.println("Factory: End.\n");
         return strategy;
     }
 }
